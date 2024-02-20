4752a7cf667ef8e1039d1d7fa0e4a805cc28359a
brandenreilly <155998782+brandenreilly@users.noreply.github.com>
2024-02-20 22:14:14 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -4,8 +4,43 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { bottom } from "@popperjs/core";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("everything starts with a Hello World :)");
+  /*  document.querySelector(".topLeftSuit").innerHTML = suitGenerator() */
+  document.querySelector(".middleNumber").innerHTML =
+    middleNumber[randomCardNum];
+  suitGenerator();
+};
+let middleNumber = [2, 3, 4, 5, 6, 7, 8, 9, 10, "K", "Q", "J", "A"];
+let randomCardNum = Math.floor(Math.random() * 13);
+/* */
+let suit = ["&#9830", "&#9829", "&#9824", "&#9827"];
+let suitNum = Math.floor(Math.random() * 4);
+var topLeft = document.querySelector(".topLeftSuit");
+var bottomRight = document.querySelector(".bottomRightSuit");
+
+let suitGenerator = () => {
+  if (suitNum == 0) {
+    topLeft.innerHTML = suit[suitNum];
+    topLeft.style.color = "red";
+    bottomRight.innerHTML = suit[suitNum];
+    bottomRight.style.color = "red";
+  } else if (suitNum == 1) {
+    topLeft.innerHTML = suit[suitNum];
+    topLeft.style.color = "red";
+    bottomRight.innerHTML = suit[suitNum];
+    bottomRight.style.color = "red";
+  } else if (suitNum == 2) {
+    topLeft.innerHTML = suit[suitNum];
+    topLeft.style.color = "black";
+    bottomRight.innerHTML = suit[suitNum];
+    bottomRight.style.color = "black";
+  } else if (suitNum == 3) {
+    topLeft.innerHTML = suit[suitNum];
+    topLeft.style.color = "black";
+    bottomRight.innerHTML = suit[suitNum];
+    bottomRight.style.color = "black";
+  }
 };
