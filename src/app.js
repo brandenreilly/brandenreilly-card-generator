457bd683b9df93551a1d4a6c7e36f39314cce30f
brandenreilly <155998782+brandenreilly@users.noreply.github.com>
2024-02-21 17:56:03 +0000

    Rewrote entirety of the JS + added a call-function on a 10 second timer.

--- src/app.js
+++ src/app.js
@@ -7,43 +7,31 @@ import "./assets/img/4geeks.ico";
 //
 //
 window.onload = function() {
+  testFunction();
+}; // Calls the function testFunction everytime the page is reloaded/loaded.
+let testFunction = () => {
+  let num = document.querySelector(".middleNumber");
+  let middleNumber = [2, 3, 4, 5, 6, 7, 8, 9, 10, "K", "Q", "J", "A"];
+  let randomCardNum = Math.floor(Math.random() * 13);
+  let suit = ["&#9830", "&#9829", "&#9824", "&#9827"];
+  let suitNum = Math.floor(Math.random() * 4);
+  let topLeft = document.querySelector(".topLeftSuit");
+  let bottomRight = document.querySelector(".bottomRightSuit");
+  topLeft.innerHTML = suit[suitNum];
+  bottomRight.innerHTML = suit[suitNum];
+  if (suitNum == 0 || suitNum == 1) {
+    topLeft.style.color = "red";
+    bottomRight.style.color = "red";
+    num.style.color = "red";
+  } else {
+    topLeft.style.color = "black";
+    bottomRight.style.color = "black";
+    num.style.color = "black";
+  }
   document.querySelector(".middleNumber").innerHTML =
     middleNumber[randomCardNum];
-  suitGenerator();
 };
+let newBtn = document.querySelector("#newCard");
+newBtn.addEventListener("click", testFunction); // Calls the testFunction everytime the "#newCard" button is clicked.
 //
-let middleNumber = [2, 3, 4, 5, 6, 7, 8, 9, 10, "K", "Q", "J", "A"];
-let randomCardNum = Math.floor(Math.random() * 13);
-//
-let suit = ["&#9830", "&#9829", "&#9824", "&#9827"];
-let suitNum = Math.floor(Math.random() * 4);
-var topLeft = document.querySelector(".topLeftSuit");
-var bottomRight = document.querySelector(".bottomRightSuit");
-//
-let suitGenerator = () => {
-  if (suitNum == 0) {
-    topLeft.innerHTML = suit[suitNum];
-    topLeft.style.color = "red";
-    bottomRight.innerHTML = suit[suitNum];
-    bottomRight.style.color = "red";
-  } else if (suitNum == 1) {
-    topLeft.innerHTML = suit[suitNum];
-    topLeft.style.color = "red";
-    bottomRight.innerHTML = suit[suitNum];
-    bottomRight.style.color = "red";
-  } else if (suitNum == 2) {
-    topLeft.innerHTML = suit[suitNum];
-    topLeft.style.color = "black";
-    bottomRight.innerHTML = suit[suitNum];
-    bottomRight.style.color = "black";
-  } else if (suitNum == 3) {
-    topLeft.innerHTML = suit[suitNum];
-    topLeft.style.color = "black";
-    bottomRight.innerHTML = suit[suitNum];
-    bottomRight.style.color = "black";
-  }
-};
-//
-const newCard = document.querySelector(".newCard");
-newCard.addEventListener("click", suitGenerator);
-//
+setInterval(testFunction, 10000); // Calls the testFunction every 10 seconds.
